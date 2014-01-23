3b269ed9dec33ca9144f377ce7e5944e532b3b8a
Feyruz Yalcin <fy2@sanger.ac.uk>
2014-01-23 16:38:58 +0000

    initial commit DistZilla ~.dzil configs

---
+++ profiles/default/Module.pm
@@ -0,0 +1,54 @@
+package {{$name}};
+use strict;
+use warnings;
+# VERSION
+use criticism 'brutal';
+use Moose;
+use namespace::autoclean;
+
+
+
+__PACKAGE__->meta->make_immutable;
+1;
+
+# ABSTRACT: 
+
+=head1 DESCRIPTION
+
+  .
+
+=head1 SYNOPSIS
+
+  .
+
+=method method_x
+
+  .
+
+=method method_y
+
+  .
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+  .
+
+=head1 DEPENDENCIES
+
+  .  
+
+=head1 INCOMPATIBILITIES
+
+  .
+ 
+=head1 DIAGNOSTICS
+
+  .
+
+=head1 BUGS AND LIMITATIONS
+
+  .
+
+=head1 SEE ALSO
+
+  .
